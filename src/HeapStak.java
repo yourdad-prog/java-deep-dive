fab0ed2f0c3aa4977c22f91fc6a673e2bfb17c56
Harshil <harshilprajapati617@gmail.com>
2026-06-18 11:59:37 +0530

    heap and stack, memory management

---
+++ src/HeapStak.java
@@ -0,0 +1,42 @@
+public class HeapStak {
+    public static void main(String args[]) {
+        Change c = new Change(10,20);
+        changeVal(c);
+        System.out.println(c.getNum());
+
+
+
+    }
+    public static void changeVal(Change c){
+        c.setNum(200);
+    }
+
+}
+
+class Change{
+    private int num;
+    private int num2;
+
+
+
+    public Change(int num, int num2){
+        this.num = num;
+        this.num2 = num2;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void setNum2(int num2){
+        this.num2 = num2;
+    }
+}
